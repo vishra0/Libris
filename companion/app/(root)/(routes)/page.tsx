4c23c377b006cfb46f21925d7f886432cd7ed436
Vishnu Rao L <vraolalige@gmail.com>
2025-04-19 23:19:53 +0530

    db integration

--- companion/app/(root)/(routes)/page.tsx
+++ companion/app/(root)/(routes)/page.tsx
@@ -1,11 +1,15 @@
 import SearchInput from "@/components/search-input";
 import { UserButton } from "@clerk/nextjs";
 import { User } from "lucide-react";
+import prismadb from "@/lib/prismadb";
+import { Categories } from "@/components/categories";
 
-const RootPage = () => {
+const RootPage = async () => {
+  const categories = await prismadb.category.findMany()
   return ( 
     <div className="h-full p-4 space-y-2">
       <SearchInput />
+      <Categories data={categories} />
     </div>
    );
 }
